1a656a702cbc7ea9ba86dda1387a472dbb3b7b36
SS518 <72257999+SS518@users.noreply.github.com>
2021-04-29 08:22:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var player1,player2;
+var player1IMGforward,player1IMGback;
+function preload (){
+player1IMGforward = loadAnimations(white/forward1.gif,white/forward2.gif,white/forward3.gif,white/forward4.gif,white/forward5.gif,white/forward6.gif,white/forward7.gif,white/forward8.gif,white/forward9.gif,white/forward10.gif,)
+player1IMGback = loadAnimations(white/back1.gif,white/back2.gif,white/back3.gif,white/back4.gif,white/back5.gif,white/back6.gif,white/back7.gif,white/back8.gif,white/back9.gif,white/back10.gif,white/back11.gif,white/back12.gif,white/back13.gif,white/back14.gif,white/back15.gif,white/back16.gif,white/back17.gif,white/back18.gif,)
+}
+
+function setup (){
+
+}
+
+function draw (){
+
+}
